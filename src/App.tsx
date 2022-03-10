8719a2c29db393285fcd36c31e5244eaa91c7b11
AC Guerrero <alex.lauks.developer@pm.me>
2022-03-10 15:43:27 +0100

    fix socials

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,40 @@ function App() {
       <video autoPlay loop muted>
         <source src="/tilect_prueba02.mp4" type="video/mp4" />
       </video>
-      <div className="App-socials">
-        <div>
-          <a
-            href="https://t.me/tilectartpublic"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Icon icon="telegram" />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://instagram.com/tilect.art"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Icon icon="instagram" />
-          </a>
-        </div>
-        <div>
-          <a href="https://github.com/tilect" target="_blank" rel="noreferrer">
-            <Icon icon="github" />
-          </a>
-        </div>
-        <div>
-          <button disabled>
-            <Icon icon="soundcloud" color="gray" />
-          </button>
+      <div>
+        <div className="App-socials-grid">
+          <div>
+            <a
+              href="https://t.me/tilectartpublic"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icon icon="telegram" />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://instagram.com/tilect.art"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icon icon="instagram" />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://github.com/tilect"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icon icon="github" />
+            </a>
+          </div>
+          <div>
+            <button disabled>
+              <Icon icon="soundcloud" color="gray" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
